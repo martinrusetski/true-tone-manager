5f68be1d1b5838dbac53f2f809a8e7f25568bd7e
mrtnby <martin.rusiecki@gmail.com>
2026-07-08 17:01:53 +0200

    Update cask sha256 for reissued v0.3.0 DMG with new app icon

--- Casks/true-tone-manager.rb
+++ Casks/true-tone-manager.rb
@@ -1,6 +1,6 @@
 cask "true-tone-manager" do
   version "0.3.0"
-  sha256 "63423505ac6243ec75b3806502c7e97bbc42ad005f662af3d810760078c55f5c"
+  sha256 "efec134987e6a709231b572185177444c1c8b14fc17fec21ed725fd373026f5a"
 
   url "https://github.com/martinrusetski/true-tone-manager/releases/download/v0.3.0/TrueToneManager-v0.3.0.dmg"
   name "TrueTone Manager"
